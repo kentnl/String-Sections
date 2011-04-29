df3ddbf14de413d6c549b966011a98066f7aee7e
Kent Fredric <kentfredric@gmail.com>
2011-04-30 11:31:36 +1200

    mostly working code

--- lib/String/Sections.pm
+++ lib/String/Sections.pm
@@ -29,7 +29,7 @@ This module is designed to behave as a work-alike, except on already extracted s
   #
   # $sections->merge( $other_sections_object );
 
-  my $section_names = $sections->section_names();
+  my @section_names = $sections->section_names();
   if ( $sections->has_section( 'section_label' ) ) {
     my $string_ref = $sections->section( 'section_label' );
     ...
@@ -39,7 +39,7 @@ This module is designed to behave as a work-alike, except on already extracted s
 
 use 5.010001;
 use Scalar::Util qw( blessed );
-use Params::Classify qw( check_regexp );
+use Params::Classify qw( check_regexp check_string );
 use Carp qw();
 use Package::Stash;
 use Sub::Name;
@@ -83,6 +83,7 @@ sub __attr_list {
         if (@args) {
           my ($value) = $args[0];
           {
+            q
             local $_ = $value;
             $validator->($_);
           }
@@ -125,10 +126,56 @@ sub new {
   my $object = bless $config, $class;
   return $object;
 }
+use Data::Dump qw( dump );
 
 sub load_list {
   my ( $self, @rest ) = @_;
   @rest = @{ $rest[0] } if ref $rest[0] and ref $rest[0] eq 'ARRAY';
+  my %stash;
+  my $current;
+
+  if ( $self->_default_name ) {
+    my $blank = q{};
+    $current = $self->_default_name;
+    $stash{ $self->_default_name } = \$blank;
+  }
+
+LINE: for my $line (@rest) {
+
+    if ( $line =~ $self->_header_regex ) {
+      my $blank = q{};
+      $current = $1;
+      $stash{$current} = \$blank;
+      next LINE;
+    }
+
+    # not valid for lists because lists are not likely to have __END__ in them.
+    if ( $self->_stop_at_end ) {
+      last LINE if $line =~ $self->_document_end_regex;
+    }
+
+    if ( $self->_ignore_empty_prelude ) {
+      next LINE if not defined $current and $line =~ $self->_empty_line_regex;
+    }
+
+    if ( not defined $current ) {
+      Carp::confess(
+        'bogus data section: text outside named section. line: ' . $line
+
+          #. ' self: ' . dump $self
+      );
+    }
+
+    if ( $self->_enable_escapes ) {
+      my $regex = $self->_line_escape_regex;
+      $line =~ s{$regex}{}x;
+    }
+
+    ${ $stash{$current} } .= $line;
+  }
+
+  $self->{stash}     = \%stash;
+  $self->{populated} = 1;
   return 1;
 }
 
@@ -183,16 +230,48 @@ sub _store_stash {
   return $value;
 }
 
+#
+# Defines accessors *_regex and _*_regex , that are for public and private access respectively.
+# Defaults to _default_*_regex.
+#
 __attr_list(
   sub {
     sub { check_regex($_) }
   },
-  qw( header_regex empty_line_regex document_end_regex )
+  qw( header_regex empty_line_regex document_end_regex line_escape_regex )
 );
 
+# String | Undef accessors.
+#
+__attr_list(
+  sub {
+    sub { not defined $_ or check_string($_) }
+  },
+  qw( default_name ),
+);
+
+# Boolean Accessors
+__attr_list(
+  sub {
+    sub { 1; }
+  },
+  qw( stop_at_end ignore_empty_prelude enable_escapes ),
+);
+
+# Go through all these methods and curry them to do method checks.
+
 __method_list(
-  qw( load_list load_string load_filehandle merge section_names has_section section _sections _stash _store_stash
-    header_regex _header_regex empty_line_regex _empty_line_regex document_end_regex _document_end_regex )
+  qw(
+    load_list load_string load_filehandle merge section_names has_section section _sections _stash _store_stash
+    header_regex _header_regex
+    empty_line_regex _empty_line_regex
+    document_end_regex _document_end_regex
+    line_escape_regex  _line_escape_regex
+    default_name _default_name
+    stop_at_end _stop_at_end
+    ignore_empty_prelude _ignore_empty_prelude
+    enable_escapes _enable_escapes
+    )
 );
 
 sub _default_header_regex {
@@ -221,6 +300,29 @@ sub _default_document_end_regex {
     ^          # Start of line
     __END__    # Document END matcher
   }msx;
+}
+
+sub _default_line_escape_regex {
+  return qr{
+    \A  # Start of line
+    \\  # A literal \
+  }msx;
+}
+
+sub _default_default_name {
+  return;
+}
+
+sub _default_stop_at_end {
+  return;
+}
+
+sub _default_ignore_empty_prelude {
+  return 1;
+}
+
+sub _default_enable_escapes {
+  return undef;
 }
 
 1;
